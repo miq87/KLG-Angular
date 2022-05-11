c2cbe57f711d6fc93c35311b42404d13f284e931
MIQ87 <miq3l@wp.pl>
2022-05-12 00:49:20 +0200

    form fixed

--- src/app/components/second-page/second-page.component.ts
+++ src/app/components/second-page/second-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { flatMap, map } from 'rxjs';
 
 @Component({
   selector: 'app-second-page',
